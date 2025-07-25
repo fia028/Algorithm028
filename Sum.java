9db853b366619477466d860d44f016106762b372
fia028” <684259028@webmail.npru.ac.th>
2025-07-25 15:22:53 +0700

    import Num1andnum2

--- Sum.java
+++ Sum.java
@@ -1,6 +1,19 @@
-Public class Sum{
-    int num1 = 4;
-    int num2 = 6;
-    int sum = num1+num2;
-    System.out.println ("sum of "+ num1 + "and"+ num2 +"=" +sum);
+import java.until.Scanner;
+
+public class sum {
+    public static void main (string args[]) {
+        // int num1 = 4;
+        // int num2 = 6;
+
+        Scanner Scanner=new Scanner(System.in);
+
+        System.out.print("Enter num1: ");
+        int num1 = Scanner.nextint();
+
+        System.out.print ("Enter num2: ");
+        int num2 = Scanner.nextint();
+
+        int sum = num1+num2;
+        System.out.println("sum of " +num1 + " and " +num2 +" = " +sum);
+    }
 }
